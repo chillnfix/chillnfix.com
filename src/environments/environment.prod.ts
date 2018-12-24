fe1f942ba5e7b917aec9300b008bd819485a9e0e
Ubaid Azad <ubaid.azad@paytmmall.com>
2018-12-24 21:37:22 +0530

    configure travis-ci build process.
    configure travis-ci build
    
    ci(travis): make .sh files executable
    
    fix(travis-ci): create log dir before using it.
    
    fix(travis-ci): build configration fix
    
    fix(build): make prod build stable.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
+
+const FIREBASE_DOMAIN = 'https://us-central1-chillnfix-dev';
+
 export const environment = {
-  production: true
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyDmuamS5Fe_WcLnl8-EgA731dPR7tOdIEg",
+        authDomain: "chillnfix-dev.firebaseapp.com",
+        databaseURL: "https://chillnfix-dev.firebaseio.com",
+        projectId: "chillnfix-dev",
+    },
+    firebaseDomain: FIREBASE_DOMAIN,
+    cloudFunctionsDomain: `${FIREBASE_DOMAIN}/cloudfunctions.net`
 };
