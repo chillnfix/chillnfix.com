58a72792d5cd41293f9ca2b7ed2e7622f171230d
Ubaid Azad <ubaid.azad@paytmmall.com>
2019-01-01 12:53:43 +0530

    perf: perf improvement in js bundles.
    perf: perf improvement in main js bundle.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,39 +7,30 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
 import { MaterialModule } from './material/material.module';
-import { CreateGroupDialogComponent } from './create-group-dialog/create-group-dialog.component';
-import { FormsModule } from '@angular/forms';
 import { httpInterceptorProviders } from './http-interceptors';
 import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CreateGroupDialogComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    FormsModule,
     // firebase modules
-    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireModule.initializeApp(environment.firebase, 'chillnfix'),
     AngularFireAuthModule,
-    AngularFirestoreModule,
 
     MaterialModule
   ],
   providers: [
     httpInterceptorProviders
   ],
-  bootstrap: [AppComponent],
-   entryComponents: [
-    CreateGroupDialogComponent
-  ]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/group-list/group-list.module.ts
+++ src/app/group-list/group-list.module.ts
@@ -5,14 +5,19 @@ import { GroupListRoutingModule } from './group-list-routing.module';
 import { GroupListComponent } from './group-list/group-list.component';
 import { MaterialModule } from '../material/material.module';
 import { FormsModule } from '@angular/forms';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { CreateGroupDialogComponent } from './create-group-dialog/create-group-dialog.component';
 
 @NgModule({
   imports: [
     CommonModule,
     MaterialModule,
     FormsModule,
-    GroupListRoutingModule
+    GroupListRoutingModule,
+
+    AngularFirestoreModule
   ],
-  declarations: [GroupListComponent]
+  declarations: [GroupListComponent, CreateGroupDialogComponent],
+  entryComponents: [CreateGroupDialogComponent]
 })
 export class GroupListModule { }
